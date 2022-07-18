e2b8a457227cb54412ee043a86c45a49a553291d
Leo Champion <92898263+leochampz@users.noreply.github.com>
2022-07-17 23:49:10 -0300

    Add files via upload
    A simple Python script witha welcome to my github, expect much more here!!!

---
+++ Welcome.py
@@ -0,0 +1,16 @@
+#Um script simples para iniciar minha conta no github
+#by Ch4mpz
+n = str(input('Qual o seu nome? '))
+import time
+time.sleep(1)
+print('=' *20)
+time.sleep(1)
+print('      BEM-VINDO   ')
+time.sleep(1)
+print('=' *20)
+time.sleep(1)
+print('\n')
+print('Olá', n, 'Seja bem vindo ao primeiro script by Ch4mpz')
+time.sleep(1)
+print('\n')
+print('C-Ya!')
